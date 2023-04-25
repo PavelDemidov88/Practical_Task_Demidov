96ac5890a426d30825f5f1c7cb73f9dbfd24a811
Pavel <alopavlo@mail.ru>
2023-04-25 20:46:08 +0300

    Задача 4

---
+++ Task1.4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int a = new Random().Next(0, 100);
+int b = new Random().Next(0, 100);
+int c = new Random().Next(0, 100);
+Console.WriteLine("" +a);
+Console.WriteLine("" +b);
+Console.WriteLine("" +c);
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine("Max: " +max);
+
